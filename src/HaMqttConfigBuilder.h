54891308cd3387f6b90d97567532cb7ce3fdb689
Lübbe Onken <luebbe.opensource@gmail.com>
2023-09-18 17:53:56 +0200

    Extend add string function to allow for nested json/arraysAdd optional parameter which will not be added, when the value is empty

--- src/HaMqttConfigBuilder.h
+++ src/HaMqttConfigBuilder.h
@@ -1,10 +1,12 @@
 #include "Arduino.h"
 
 // An helper class to generate a json Key/Value config payload for Home Assistant
-class HaMqttConfigBuilder {
+class HaMqttConfigBuilder
+{
 
 private:
-  struct Elem {
+  struct Elem
+  {
     String key;
     String str;
     double num;
@@ -12,12 +14,17 @@ private:
   std::vector<Elem> elements;
 
 public:
-  // Add an element to the config paylaod
-  HaMqttConfigBuilder& add(const String& key, const String& str) {
-    elements.push_back({key, str, 0});
+  // Add an element to the config payload if it's not empty
+  HaMqttConfigBuilder &add(const String &key, const String &str, bool isRequired = true)
+  {
+    if (isRequired || (str.length() > 0))
+    {
+      elements.push_back({key, str, 0});
+    }
     return *this;
   }
-  HaMqttConfigBuilder& add(const String& key, const double num) {
+  HaMqttConfigBuilder &add(const String &key, const double num)
+  {
     elements.push_back({key, "", num});
     return *this;
   }
@@ -26,26 +33,35 @@ public:
   void clear() { elements.clear(); };
 
   // Generate the json key/value payload for home assistant
-  String generatePayload() {
+  String generatePayload()
+  {
     String str = "{";
 
-    for(Elem elem : elements) {
+    for (Elem elem : elements)
+    {
       str.concat('"');
       str.concat(elem.key);
       str.concat("\":");
-      
-      if (elem.str.length() > 0) {
+
+      if (elem.str.startsWith("{") || elem.str.startsWith("["))
+      {
+        str.concat(elem.str);
+      }
+      else if (elem.str.length() > 0)
+      {
         str.concat('"');
         str.concat(elem.str);
         str.concat('"');
-      } else {
+      }
+      else
+      {
         str.concat(elem.num);
       }
       str.concat(',');
     }
 
     if (str.endsWith(","))
-      str.setCharAt(str.length()-1, '}');
+      str.setCharAt(str.length() - 1, '}');
     else
       str.concat('}');
 

999d28cd8fd4afa56961e7093819376c5f3e6ef2
Lübbe Onken <luebbe.opensource@gmail.com>
2024-01-07 07:46:17 +0100

    Implement a subclass that handles creation of homeassistant autodiscovery entities for light, sensor and select

--- src/HaMqttConfigBuilder.cpp
+++ src/HaMqttConfigBuilder.cpp
@@ -1,86 +1,5 @@
 #include "HaMqttConfigBuilder.h"
 
-HaMqttConfigBuilder::HaMqttConfigBuilder(
-    const char *uniqueId,
-    const char *fwName,
-    const char *fwVersion,
-    const char *fwManufacturer,
-    const char *fwModel,
-    const char *mqttPrefix,
-    const char *availabilityTopic,
-    const char *payloadAvailable,
-    const char *payloadNotAvailable) : _uniqueId(uniqueId),
-                                       _mqttPrefix(mqttPrefix),
-                                       _availabilityTopic(String("~/") + availabilityTopic),
-                                       _payloadAvailable(payloadAvailable),
-                                       _payloadNotAvailable(payloadNotAvailable)
-{
-    _deviceId = _uniqueId.substring(6);
-    _deviceInfo = add("ids", _uniqueId)
-                      .add("name", fwName)
-                      .add("sw", fwVersion)
-                      .add("mf", fwManufacturer)
-                      .add("mdl", fwModel)
-                      .generatePayload();
-    Serial.println(_uniqueId);
-    Serial.println(_deviceId);
-}
-
-HaMqttConfigBuilder &HaMqttConfigBuilder::addDefaults(const char *name, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass)
-{
-    String uniq_id = id;
-    uniq_id.toLowerCase();
-    uniq_id.replace(" ", "_");
-
-    clear();
-    return add("name", name)
-        .add("uniq_id", String(_deviceId) + String("_") + uniq_id)
-        .add("dev_cla", deviceClass, false)
-        .add("ic", icon, false)
-        .add("unit_of_meas", unit, false)
-        .addSource("dev", _deviceInfo)
-        .add("~", _mqttPrefix)
-        .add("avty_t", _availabilityTopic)
-        .add("pl_avail", _payloadAvailable)
-        .add("pl_not_avail", _payloadNotAvailable)
-        .add("stat_t", String("~/") + stateTopic);
-}
-
-String HaMqttConfigBuilder::createLight(const char *friendlyName, const char *id, const char *stateTopic, const char *icon)
-{
-    String config =
-        addDefaults(friendlyName, id, stateTopic, "", icon, "")
-            .add("cmd_t", String("~/") + stateTopic + String("/set"))
-            .add("pl_off", "Off")
-            .add("pl_on", "On")
-            .generatePayload();
-
-    sendConfig("light", id, config);
-    return config;
-}
-
-String HaMqttConfigBuilder::createSelect(const char *friendlyName, const char *id, const char *stateTopic, const char *icon, const char *options)
-{
-    String config =
-        addDefaults(friendlyName, id, stateTopic, "", icon, "")
-            .addSource("options", options)
-            .add("cmd_t", String("~/") + stateTopic + String("/set"))
-            .generatePayload();
-
-    sendConfig("select", id, config);
-    return config;
-}
-
-String HaMqttConfigBuilder::createSensor(const char *friendlyName, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass)
-{
-    String config =
-        addDefaults(friendlyName, id, stateTopic, unit, icon, deviceClass)
-            .generatePayload();
-
-    sendConfig("sensor", id, config);
-    return config;
-}
-
 String HaMqttConfigBuilder::generatePayload()
 {
     String payload = "{";
@@ -114,11 +33,83 @@ String HaMqttConfigBuilder::generatePayload()
     return payload;
 }
 
-void HaMqttConfigBuilder::sendConfig(const char *confType, const char *id, const String &config)
+DeviceConfigBuilder::DeviceConfigBuilder(
+    const char *uniqueId,
+    const char *fwName,
+    const char *fwVersion,
+    const char *fwManufacturer,
+    const char *fwModel) : _uniqueId(uniqueId)
+{
+    _deviceId = _uniqueId.substring(6); // Copy the second half of the MAC address to use it as the short ID
+    _deviceInfo =
+        HaMqttConfigBuilder::add("ids", _uniqueId)
+            .add("name", fwName)
+            .add("sw", fwVersion)
+            .add("mf", fwManufacturer)
+            .add("mdl", fwModel)
+            .generatePayload();
+}
+
+HaMqttConfigBuilder &DeviceConfigBuilder::addDefaults(const char *name, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass)
+{
+    String uniq_id = id;
+    uniq_id.toLowerCase();
+    uniq_id.replace(" ", "_");
+
+    clear();
+    return add("name", name)
+        .add("uniq_id", String(_deviceId) + String("_") + uniq_id)
+        .add("dev_cla", deviceClass, false)
+        .add("ic", icon, false)
+        .add("unit_of_meas", unit, false)
+        .addSource("dev", _deviceInfo)
+        .add("~", _deviceTopic)
+        .add("avty_t", String("~/") + _availabilityTopic)
+        .add("pl_avail", _payloadAvailable)
+        .add("pl_not_avail", _payloadNotAvailable)
+        .add("stat_t", String("~/") + stateTopic);
+}
+
+String DeviceConfigBuilder::createLight(const char *friendlyName, const char *id, const char *stateTopic, const char *icon)
+{
+    String config =
+        addDefaults(friendlyName, id, stateTopic, "", icon, "")
+            .add("cmd_t", String("~/") + stateTopic + String("/set"))
+            .add("pl_off", "Off")
+            .add("pl_on", "On")
+            .generatePayload();
+
+    sendConfig("light", id, config);
+    return config;
+}
+
+String DeviceConfigBuilder::createSelect(const char *friendlyName, const char *id, const char *stateTopic, const char *icon, const char *options)
+{
+    String config =
+        addDefaults(friendlyName, id, stateTopic, "", icon, "")
+            .addSource("options", options)
+            .add("cmd_t", String("~/") + stateTopic + String("/set"))
+            .generatePayload();
+
+    sendConfig("select", id, config);
+    return config;
+}
+
+String DeviceConfigBuilder::createSensor(const char *friendlyName, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass)
+{
+    String config =
+        addDefaults(friendlyName, id, stateTopic, unit, icon, deviceClass)
+            .generatePayload();
+
+    sendConfig("sensor", id, config);
+    return config;
+}
+
+void DeviceConfigBuilder::sendConfig(const char *confType, const char *id, const String &config)
 {
     const uint8_t MAX_MQTT_LENGTH = 255;
     char mqttTopic[MAX_MQTT_LENGTH];
-    snprintf(mqttTopic, MAX_MQTT_LENGTH, cHaDiscoveryTopic "/%s/%s/%s/config", confType, _mqttPrefix.c_str(), id);
+    snprintf(mqttTopic, MAX_MQTT_LENGTH, "%s/%s/%s/%s/config", _discoveryTopic.c_str(), confType, _deviceTopic.c_str(), id);
 
     if (_sendCallback)
     {
--- src/HaMqttConfigBuilder.h
+++ src/HaMqttConfigBuilder.h
@@ -1,9 +1,20 @@
+#pragma once
+
 #include "Arduino.h"
 
-// An helper class to generate a json Key/Value config payload for Home Assistant
+// Home Assistant integration
+
+#define cDiscoveryTopic "homeassistant"
+#define cDeviceTopic  "device"
+#define cAvailabilityTopic "$state"
+#define cPlAvailable "ready"
+#define cPlNotAvailable "lost"
+
+typedef std::function<void(const char *topic, const char *payload)> SendCallback;
+
+// A helper class to generate a json Key/Value config payload for Home Assistant
 class HaMqttConfigBuilder
 {
-
 private:
   struct Elem
   {
@@ -12,7 +23,7 @@ private:
     double num;
     boolean asSource;
   };
-  std::vector<Elem> elements;
+  std::vector<Elem> _elements;
 
 public:
   // Add an element to the config payload if it's not empty or if it is required
@@ -20,57 +31,87 @@ public:
   {
     if (isRequired || (str.length() > 0))
     {
-      elements.push_back({key, str, 0, false});
+      _elements.push_back({key, str, 0, false});
     }
     return *this;
   }
 
   HaMqttConfigBuilder &add(const String &key, const double num)
   {
-    elements.push_back({key, "", num, false});
+    _elements.push_back({key, "", num, false});
     return *this;
   }
 
   HaMqttConfigBuilder &addSource(const String &key, const String &str)
   {
-    elements.push_back({key, str, 0, true});
+    _elements.push_back({key, str, 0, true});
     return *this;
   }
 
   // Clear all elements
-  void clear() { elements.clear(); };
+  void clear() { _elements.clear(); };
 
   // Generate the json key/value payload for home assistant
-  String generatePayload()
+  String generatePayload();
+};
+
+class DeviceConfigBuilder : HaMqttConfigBuilder
+{
+private:
+  String _uniqueId = "";
+  String _deviceId = "";
+  String _deviceInfo = "";
+  String _discoveryTopic = cDiscoveryTopic;
+  String _deviceTopic = cDeviceTopic;
+  String _availabilityTopic = cAvailabilityTopic;
+  String _payloadAvailable = cPlAvailable;
+  String _payloadNotAvailable = cPlNotAvailable;
+
+  SendCallback _sendCallback = NULL;
+
+  HaMqttConfigBuilder &addDefaults(const char *name, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass);
+  void sendConfig(const char *confType, const char *name, const String &config);
+
+public:
+  DeviceConfigBuilder(
+      const char *uniqueId,
+      const char *fwName,
+      const char *fwVersion,
+      const char *fwManufacturer,
+      const char *fwModel);
+
+  // Create different types of home assistant auto configurations
+  String createLight(const char *name, const char *id, const char *stateTopic, const char *icon);
+  String createSelect(const char *name, const char *id, const char *stateTopic, const char *icon, const char *options);
+  String createSensor(const char *name, const char *id, const char *stateTopic, const char *icon, const char *unit, const char *deviceClass);
+
+  DeviceConfigBuilder setDeviceTopic(char *deviceTopic)
   {
-    String str = "{";
+    _deviceTopic = deviceTopic;
+    return *this;
+  }
 
-    for (Elem elem : elements)
-    {
-      str.concat('"');
-      str.concat(elem.key);
-      str.concat("\":");
+  DeviceConfigBuilder setAvailabilityTopic(const char *availabilityTopic)
+  {
+    _availabilityTopic = availabilityTopic;
+    return *this;
+  }
 
-      if (elem.str.length() > 0)
-      {
-        if (!elem.asSource)
-          str.concat('"');
-        str.concat(elem.str);
-        if (!elem.asSource)
-          str.concat('"');
-      }
-      else
-      {
-        str.concat(elem.num);
-      }
-      str.concat(',');
-    }
+  DeviceConfigBuilder setPayloadAvailable(const char *payloadAvailable)
+  {
+    _payloadAvailable = payloadAvailable;
+    return *this;
+  }
 
-    if (str.endsWith(","))
-      str.setCharAt(str.length() - 1, '}');
-    else
-      str.concat('}');
+  DeviceConfigBuilder setPayloadNotAvailable(const char *payloadNotAvailable)
+  {
+    _payloadNotAvailable = payloadNotAvailable;
+    return *this;
+  }
 
-    return str;
+  DeviceConfigBuilder setSendCallback(SendCallback sendCallback)
+  {
+    _sendCallback = sendCallback;
+    return *this;
   }
 };
